b7448e4ea961e7c36e53d92795213930c711c3e8
GlennWSo <sondellglenn@gmail.com>
2024-02-06 16:35:55 +0100

    bump assigenment version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '0684f88ec63fd8514807016251c0094fce230069'
+AESD_ASSIGNMENTS_VERSION = '789ffbc8ee9663918fbcaa745553b1906bdbaadb'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
